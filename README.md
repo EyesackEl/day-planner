75de53b793534aa55ee46b1b54853cce2ab6d33d
Eyesack <isaac.laflamme@gmail.com>
2022-05-05 14:07:12 -0700

    finished java css and readme, potentially final commit!

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # day-planner
+
+## Description
+    This page is a day planner that updates its css dynamically based on the time of day. With it a user can save their daily to-dos locally and have a visual aid of how much of their work day is left.
+
+## Usage
+    To use the application a user must simply click on the text area in the hour row of their choosing and add or delete text to their hearts content. When done, clicking the 'save' button will save the text to local storage to be accessed later without keeping the page open in their browser.
+
+    Sceeenshot of application below:
+
+![screenshot](./docs/assets/style/images/screenshot-of-deployed.PNG)
+
+## Deployed Application Link
+https://eyesackel.github.io/day-planner
--- docs/assets/script/script.js
+++ docs/assets/script/script.js
@@ -1,18 +1,22 @@
 var currentDay = moment().format("dddd, MMMM Do");
 var currentHour = parseInt(moment().format("k"));
+var inputBoxes = document.getElementsByClassName("input")
 var saveBtn = document.getElementsByTagName("BUTTON");
 var hourRows = document.getElementsByClassName("custom-row")
 
 
-console.log("Hour Rows: " , hourRows)
+// Setting the current day at the top of the page
 $("#currentDay").text(currentDay);
-
+console.log("Input Boxes: ", inputBoxes)
+console.log("Hour Rows: " , hourRows)
 console.log("Current Hour: " + currentHour);
 
-//Wrapping setting backgroudn color in timer to update css dynamically
-//as hour changes
+// Wrapping background color in timer to update css dynamically
+// as hour changes
 timer = setInterval(function() {
-    //Sets background color for past, present, and future timeblocks
+    // Reseting classes
+    $(hourRows).removeClass(["future", "present", "past"]);
+    // Sets background color for past, present, and future timeblocks
     if (currentHour < 9) {
         $(hourRows).addClass("future");
 
@@ -123,12 +127,50 @@ timer = setInterval(function() {
             $(hourRows[i]).removeClass("present");
             $(hourRows[i]).addClass("past");
         }
-}
-
+    }
 }, 1)
 
-
-
-$(saveBtn).click(function() {
-
+// Saving the text the user inputs to local storage
+$(saveBtn[0]).click(function() {
+    console.log($(inputBoxes[0]).val())
+    localStorage.setItem("input0", $(inputBoxes[0]).val());
 })
+$(saveBtn[1]).click(function() {
+    localStorage.setItem("input1", $(inputBoxes[1]).val());
+})
+$(saveBtn[2]).click(function() {
+    localStorage.setItem("input2", $(inputBoxes[2]).val());
+})
+$(saveBtn[3]).click(function() {
+    localStorage.setItem("input3", $(inputBoxes[3]).val());
+})
+$(saveBtn[4]).click(function() {
+    localStorage.setItem("input4", $(inputBoxes[4]).val());
+})
+$(saveBtn[5]).click(function() {
+    localStorage.setItem("input5", $(inputBoxes[5]).val());
+})
+$(saveBtn[6]).click(function() {
+    localStorage.setItem("input6", $(inputBoxes[6]).val());
+})
+$(saveBtn[7]).click(function() {
+    localStorage.setItem("input7", $(inputBoxes[7]).val());
+})
+$(saveBtn[8]).click(function() {
+    localStorage.setItem("input8", $(inputBoxes[8]).val());
+})
+
+// Renders users previously entered text from local storage when page loads
+function renderLists() {
+        $(inputBoxes[0]).val(localStorage.getItem("input0"))
+        $(inputBoxes[1]).val(localStorage.getItem("input1"))
+        $(inputBoxes[2]).val(localStorage.getItem("input2"))
+        $(inputBoxes[3]).val(localStorage.getItem("input3"))
+        $(inputBoxes[4]).val(localStorage.getItem("input4"))
+        $(inputBoxes[5]).val(localStorage.getItem("input5"))
+        $(inputBoxes[6]).val(localStorage.getItem("input6"))
+        $(inputBoxes[7]).val(localStorage.getItem("input7"))
+        $(inputBoxes[8]).val(localStorage.getItem("input8"))
+}
+
+renderLists();
